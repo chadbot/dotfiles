510522fb54d2e528f586e6db4e669adcdb0ed2b2
Chad Borkenhagen <chad.borkenhagen@gmail.com>
2023-12-18 20:01:23 -0500

    Install script checks for existing .dotfiles dir first.

--- .config/install/install.zsh
+++ .config/install/install.zsh
@@ -1,19 +1,30 @@
 
 # Move any existing dotfiles files into backup directory.
 DATE=`date +"%Y%m%d"`
+DOTFILES_DIR=$HOME/.dotfiles
 BACKUP_DIR=$HOME/dotfiles_backup_$DATE
+
+# Check if dotfiles folder already exists, and quit if it does.
+if [ -d "$DOTFILES_DIR" ];
+then
+    echo ".dotfiles directory already exists. Rename or move directory and try again."
+    return
+fi 
+
 if [ -d "$BACKUP_DIR" ];
 then
     echo "Can't backup dotfiles. $BACKUP_DIR directory already exists." 
     echo "Rename or move this directory and try installing again."
-    rm -rf $HOME/.dotfiles
     return
 else
     mkdir $BACKUP_DIR
 fi
 
+# Clone bare repo.
+git clone --bare https://github.com/chadbot/dotfiles.git $DOTFILES_DIR
+
 # Copy existing files. Rsync preserves paths.
-for file in $(git -C $HOME/.dotfiles ls-tree -r --full-name --name-only HEAD)
+for file in $(git -C $DOTFILES_DIR ls-tree -r --full-name --name-only HEAD)
     do 
         if [ -f $file ];
             then
@@ -22,11 +33,8 @@ for file in $(git -C $HOME/.dotfiles ls-tree -r --full-name --name-only HEAD)
         fi
 done
 
-# Clone bare repo.
-git clone --bare https://github.com/chadbot/dotfiles.git $HOME/.dotfiles
-
 # Define dotfiles alias.
-alias dotfiles="git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
+alias dotfiles="git --git-dir=$DOTFILES_DIR --work-tree=$HOME"
 
 # Checkout files.
 dotfiles checkout
